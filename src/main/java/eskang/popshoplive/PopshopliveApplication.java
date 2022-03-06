06433b6d09bb3c2247c2c3779045f376c17ca5c3
Eunseok Kang <kes0228@gmail.com>
2022-03-06 10:51:04 +0900

    init

---
+++ src/main/java/eskang/popshoplive/PopshopliveApplication.java
@@ -0,0 +1,14 @@
+package eskang.popshoplive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@SpringBootApplication
+public class PopshopliveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PopshopliveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eskang/popshoplive/PopshopliveApplicationTests.java
@@ -0,0 +1,13 @@
+package eskang.popshoplive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PopshopliveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
